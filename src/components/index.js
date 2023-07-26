fc79fcbbaf5acf859e97ed1059e4908e25f87671
Hossam <hossamosamaa63@gmail.com>
2023-07-26 23:10:54 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article} from './article/Article';
+export {default as Brand}  from './brand/Barnd';
+export {default as Bta} from  './bta/Bta';
+export {default as Navbar} from './navbar/Navbar';
+export {default as features} from "./feature/Feature";
